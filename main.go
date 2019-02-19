1454e656c3c6b293c4cc36521c9194c0733426e5
lima1909 <linke.mario@web.de>
2019-02-19 04:02:21 +0100

    working example with sarama-cluster

--- main.go
+++ main.go
@@ -6,61 +6,20 @@ import (
 	"os"
 	"os/signal"
 
-	"github.com/Shopify/sarama"
-
 	cluster "github.com/bsm/sarama-cluster"
 )
 
 func main() {
-	// init (custom) config, set mode to ConsumerModePartitions
-	config := cluster.NewConfig()
-	config.Group.Mode = cluster.ConsumerModePartitions
-
-	// init consumer
 	brokers := []string{"172.22.0.4:19092", "172.22.0.5:29092"}
 	topics := []string{"foo"}
-	consumer, err := cluster.NewConsumer(brokers, "my-consumer-group", topics, config)
-	if err != nil {
-		panic(err)
-	}
-	defer consumer.Close()
 
-	// trap SIGINT to trigger a shutdown.
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, os.Interrupt)
-
-	// consume partitions
-	for {
-		select {
-		case part, ok := <-consumer.Partitions():
-			if !ok {
-				return
-			}
-
-			// start a separate goroutine to consume messages
-			go func(pc cluster.PartitionConsumer) {
-				for msg := range pc.Messages() {
-					fmt.Fprintf(os.Stdout, "%s/%d/%d\t%s\t%s\n", msg.Topic, msg.Partition, msg.Offset, msg.Key, msg.Value)
-					consumer.MarkOffset(msg, "") // mark message as processed
-				}
-			}(part)
-		case <-signals:
-			return
-		}
-	}
-
-}
-
-func _main() {
+	// init (custom) config, enable errors and notifications
 	config := cluster.NewConfig()
 	config.Consumer.Return.Errors = true
 	config.Group.Return.Notifications = true
-	config.Group.Mode = cluster.ConsumerModePartitions
-	config.Consumer.Offsets.Initial = sarama.OffsetOldest
 
-	brokers := []string{"172.22.0.4:19092", "172.22.0.5:29092"}
-	topics := []string{"foo"}
-	consumer, err := cluster.NewConsumer(brokers, "group", topics, config)
+	// init consumer
+	consumer, err := cluster.NewConsumer(brokers, "my-consumer-group", topics, config)
 	if err != nil {
 		panic(err)
 	}
